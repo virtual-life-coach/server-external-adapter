0e04f9fa555852a5da7504fd3a51520eb3ff9fe8
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2017-01-29 11:49:38 +0100

    - Quote service

--- src/main/java/vlc/ea/dispatcher/RequestDispatcher.java
+++ src/main/java/vlc/ea/dispatcher/RequestDispatcher.java
@@ -1,10 +1,31 @@
 package vlc.ea.dispatcher;
 
+import com.google.api.server.spi.response.InternalServerErrorException;
 import vlc.common.to.QuoteTO;
+import vlc.ea.quote.ForismaticQuote;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 public class RequestDispatcher {
 
-    public static QuoteTO getRandomQuote() {
-        return new QuoteTO("Scusa, non ho fantasia!"); // TODO external service API call
+    private static Logger log = Logger.getLogger(RequestDispatcher.class.getName());
+
+    public static QuoteTO getRandomQuote() throws InternalServerErrorException {
+        WebTarget resource = ClientBuilder.newClient().target("http://api.forismatic.com/api/1.0/");
+        resource = resource.queryParam("method", "getQuote").queryParam("format", "json").queryParam("lang", "en");
+        log.info("Contacting: " + resource.getUri().toString());
+        Response response = resource.request().accept(MediaType.APPLICATION_JSON).get();
+        if (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
+            response.bufferEntity();
+            ForismaticQuote forismaticQuote = response.readEntity(ForismaticQuote.class);
+            log.info("Retrieved: " + forismaticQuote);
+            return new QuoteTO(forismaticQuote.getQuoteText() + " - " + forismaticQuote.getQuoteAuthor());
+        } else {
+            throw new InternalServerErrorException("Unable to fetch quote from external service.");
+        }
     }
 }
---
+++ src/main/java/vlc/ea/quote/ForismaticQuote.java
@@ -0,0 +1,72 @@
+package vlc.ea.quote;
+
+public class ForismaticQuote {
+
+    private String quoteText;
+    private String quoteAuthor;
+    private String senderName;
+    private String senderLink;
+    private String quoteLink;
+
+    public ForismaticQuote() {
+    }
+
+    public ForismaticQuote(String quoteText, String quoteAuthor, String senderName, String senderLink, String quoteLink) {
+        this.quoteText = quoteText;
+        this.quoteAuthor = quoteAuthor;
+        this.senderName = senderName;
+        this.senderLink = senderLink;
+        this.quoteLink = quoteLink;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+
+    public String getQuoteAuthor() {
+        return quoteAuthor;
+    }
+
+    public void setQuoteAuthor(String quoteAuthor) {
+        this.quoteAuthor = quoteAuthor;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getSenderLink() {
+        return senderLink;
+    }
+
+    public void setSenderLink(String senderLink) {
+        this.senderLink = senderLink;
+    }
+
+    public String getQuoteLink() {
+        return quoteLink;
+    }
+
+    public void setQuoteLink(String quoteLink) {
+        this.quoteLink = quoteLink;
+    }
+
+    @Override
+    public String toString() {
+        return "ForismaticQuote{" +
+                "quoteText='" + quoteText + '\'' +
+                ", quoteAuthor='" + quoteAuthor + '\'' +
+                ", senderName='" + senderName + '\'' +
+                ", senderLink='" + senderLink + '\'' +
+                ", quoteLink='" + quoteLink + '\'' +
+                '}';
+    }
+}
